37aeda75f6216b2a05ea040c70fa0cfd12bf9e56
almostdevelopr <minhalam.dev@gmail.com>
2022-04-08 14:10:56 +0530

    Get previous state
    re-renders a component when it updates using useState hook

--- src/components/UseRefExample2.jsx
+++ src/components/UseRefExample2.jsx
@@ -1,7 +1,27 @@
 import { useState, useEffect, useRef } from "react";
 
 function UseRefExample2() {
-	return <div>UseRefExample2</div>;
+	const [name, setName] = useState("");
+	const renders = useRef(1); //first render
+	const prevName = useRef("");
+
+	useEffect(() => {
+		renders.current = renders.current + 1;
+		prevName.current = name;
+	}, [name]);
+
+	return (
+		<div>
+			<h1>Renders : {renders.current}</h1>
+			<h1>Prev State Name : {prevName.current}</h1>
+			<input
+				type="text"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				className="form-control mb-3"
+			/>
+		</div>
+	);
 }
 
 export default UseRefExample2;
