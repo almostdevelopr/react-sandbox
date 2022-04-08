cfb5f3a89ec0582c657941637faae8384676d1f9
almostdevelopr <minhalam.dev@gmail.com>
2022-04-08 23:18:47 +0530

    Add toggle button for a paragragh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import UseRefExample1 from "./components/UseRefExample1";
-import UseRefExample2 from "./components/UseRefExample2";
+import UseRefExample3 from "./components/UseRefExample3";
 
 function App() {
 	return (
 		<div className="App">
-			<UseRefExample2 />
+			<UseRefExample3 />
 		</div>
 	);
 }
